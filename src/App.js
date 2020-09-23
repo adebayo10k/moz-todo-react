2e967b671bde81a830c5160bde14ae452be4eadb
damola adebayo <adebayo10k@gmail.com>
2020-09-23 15:44:58 +0100

    adds event behaviours to some components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import { nanoid } from 'nanoid';
+import React, { useState } from 'react';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
 
 function App(props) {
 
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name){
-    alert(name);
+    const newTask = {id: "todo-"+nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]);
   }
 
-  console.log(props.tasks);
-  const taskList = props.tasks.map(task => (
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map( task => {
+      if (id === task.id){
+        return{...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id){
+    const remainingTasks = tasks.filter(task => task.id !== id);
+    setTasks(remainingTasks);
+  }
+
+  //console.log(tasks);
+  const taskList = tasks.map(task => (
     <Todo 
     id={task.id}
     name={task.name} 
     completed={task.completed}
-    key={task.id}  />
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}  />
   ));
-  console.log(taskList);
+  //console.log(taskList);
+
+  const taskNoun = taskList.length === 1 ? "task" : "tasks";
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -43,7 +67,7 @@ function App(props) {
         </button>
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 export default function Todo(props) {
 
-  console.log(props);
+  //console.log(props);
 
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-        <input id="todo-0" type="checkbox" defaultChecked={props.completed} />
+        <input 
+          onChange={() => props.toggleTaskCompleted(props.id)} 
+          id="todo-0" 
+          type="checkbox" 
+          defaultChecked={props.completed} 
+        />
         <label className="todo-label" htmlFor={props.id}>
           {props.name}
         </label>
@@ -16,8 +21,11 @@ export default function Todo(props) {
         <button type="button" className="btn">
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
-        <button type="button" className="btn btn__danger">
-          Delete <span className="visually-hidden">{props.name}</span>
+        <button 
+          onClick={() => props.deleteTask(props.id)} 
+          type="button" 
+          className="btn btn__danger">
+            Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
     </li>
